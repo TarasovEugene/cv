cc5d8b36a98e26ae1af055ac7c240199ac763d76
TarasovEugene <106308701+TarasovEugene@users.noreply.github.com>
2022-07-12 19:53:43 +0300

    Update js.js

--- js.js
+++ js.js
@@ -4,4 +4,4 @@ const button = document.querySelector('button');
 /* Тут на кнопку навешиваем обрабочик, который ждёт клика и тогда запустит логику */
 button.addEventListener('click', function () {
 	alert('Возможно всё!')
-})
+}) 
